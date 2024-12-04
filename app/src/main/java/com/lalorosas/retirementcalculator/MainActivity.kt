ed5a7ff5974d91cf8cccef331628e9ce4a1e264f
Felipe Wisniewski <felipewisniewski@gmail.com>
2024-12-04 11:45:22 -0300

    first commit

---
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.lalorosas.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lalorosas.retirementcalculator.databinding.ActivityMainBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        AppCenter.start(application, "4c224023-b4c3-42d6-92a0-d5f3c22d3199", Analytics::class.java, Crashes::class.java);
+
+        binding.calculateButton.setOnClickListener {
+            // Crashes.generateTestCrash()
+            try {
+                val interestRate = binding.interestEditText.text.toString().toFloat()
+                val currentAge = binding.ageEditText.text.toString().toInt()
+                val retirementAge = binding.retirementEditText.text.toString().toInt()
+                val monthly = binding.monthlySavingsEditText.text.toString().toFloat()
+                val current = binding.currentEditText.text.toString().toFloat()
+
+                val properties:HashMap<String, String> = HashMap<String, String>()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
+            } catch(ex: Exception){
+                Analytics.trackEvent(ex.message)
+            }
+        }
+    }
+}
